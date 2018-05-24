6911f5a0ed793cd2f8368ced9ba413ced1404b9f
Andrew Matsumoto <arm297@cornell.edu>
2018-05-23 23:42:31 -0400

    Added template drill for creating new drills

---
+++ Assets/Scripts/Drills/Scoring/TemplateDrillScoreHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Drills {
+
+    /**
+     * A dummy score handler for the template drill scene. To create a new
+     * drill, this script should be replaced.
+     */
+    public class TemplateDrillScoreHandler : ScoreHandler {
+        
+        // Calculates dummy 0 score.
+        public override float ComputeScore() {
+            return 0f;
+        }
+
+        // Computes max score of 1 (Prevents possible division by 0 error in
+        // dummy scene).
+        public override float ComputeMaxScore() {
+            return 1f;
+        }
+
+        // Displays info about the score. Since this is a placeholder script,
+        // there is no real info to display.
+        public override void DisplayScoreInfo() {
+            statusText.text = "TODO: Replace scoring script with a custom one\n"
+                + " and update the score handler field for drill controller";
+            scoreText.text = "No real score for this demo script";
+        }
+    
+    }
+}
